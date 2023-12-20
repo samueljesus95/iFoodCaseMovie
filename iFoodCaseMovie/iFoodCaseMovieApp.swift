bf58c88a91611142681dc6929e499cb0463f08aa
Samuel Jesus <samueljesus@live.com>
2023-12-20 20:50:27 -0300

    Initial Commit

---
+++ iFoodCaseMovie/iFoodCaseMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFoodCaseMovieApp.swift
+//  iFoodCaseMovie
+//
+//  Created by Samuel on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iFoodCaseMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
