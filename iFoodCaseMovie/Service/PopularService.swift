7d9678ede59b2c10c1cb3e31b4c3b4d239de948c
Samuel Jesus <samueljesus@live.com>
2023-12-21 20:59:29 -0300

    feat: create popular services

--- iFoodCaseMovie/Service/PopularService.swift
+++ iFoodCaseMovie/Service/PopularService.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+protocol PopularServiceProtocol {
+    func loadData(completion: @escaping (Result<[Popular], Error>) -> Void)
+}
+
+class PopularService: PopularServiceProtocol {
+    
+    //MARK: - private variables
+    private let networkManager: NetworkManagerProtocol
+    
+    //MARK: - init class
+    init(networkManager: NetworkManagerProtocol = DefaultNetworkService()) {
+        self.networkManager = networkManager
+    }
+    
+    //MARK: - public methods
+    func loadData(completion: @escaping (Result<[Popular], Error>) -> Void) {
+        let request = NetworkRequest(
+            baseURL: Constants.baseUrl,
+            path: MovieType.popular.path,
+            method: .get,
+            headers: ["Content-Type" : "application/json"],
+            queryParameters: ["api_key" : Constants.apiKey],
+            bodyParameter: nil)
+        
+        networkManager.request(request) { (_ result: Result<PopularResult, Error>) in
+            switch result {
+            case .success(let popular):
+                completion(.success(popular.results))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
