0b6f2ad78834fcbd43cf9e2ac3f67f73a9286ea9
Samuel Jesus <samueljesus@live.com>
2023-12-21 20:58:08 -0300

    feat: create popular model

--- iFoodCaseMovie/Model/Popular.swift
+++ iFoodCaseMovie/Model/Popular.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct PopularResult: Codable {
+    let page: Int
+    let total_results: Int
+    let total_pages: Int
+    let results: [Popular]
+}
+
+struct Popular: Codable, Hashable {
+    let id: Int
+    let poster_path: String?
+    let adult: Bool?
+    let overview: String?
+    let release_date: String?
+    let genre_ids: [Int]?
+    let original_title: String?
+    let original_language: String?
+    let title: String?
+    let backdrop_path: String?
+    let popularity: Double?
+    let vote_count: Int?
+    let video: Bool?
+    let vote_average: Double?
+    let total_results: Int?
+}
