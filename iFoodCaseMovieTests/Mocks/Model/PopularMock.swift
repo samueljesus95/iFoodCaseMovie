955a60f48591e42c55b4453e9a11fddbf6de4b1c
Samuel Jesus <samueljesus@live.com>
2023-12-21 21:00:39 -0300

    test: create popular services tests

--- iFoodCaseMovieTests/Mocks/Model/PopularMock.swift
+++ iFoodCaseMovieTests/Mocks/Model/PopularMock.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+@testable import iFoodCaseMovie
+
+struct PopularMock {
+    static let popularFirst = Popular(
+        id: 76600,
+        poster_path: "/5ScPNT6fHtfYJeWBajZciPV3hEL.jpg",
+        adult: false,
+        overview: "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        release_date: "2022-12-14",
+        genre_ids: [878,28,12],
+        original_title: "Avatar: The Way of Water",
+        original_language: "en",
+        title: "Avatar: The Way of Water",
+        backdrop_path: "/tQ91wWQJ2WRNDXwxuO7GCXX5VPC.jpg",
+        popularity: 4109.455,
+        vote_count: 1001,
+        video: false,
+        vote_average: 8.1,
+        total_results: nil
+    )
+
+    static let popularSecond = Popular(
+        id: 436270,
+        poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+        adult: false,
+        overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+        release_date: "2022-10-19",
+        genre_ids: [28,14,878],
+        original_title: "Black Adam",
+        original_language: "en",
+        title: "Black Adam",
+        backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+        popularity: 3189.94,
+        vote_count: 3170,
+        video: false,
+        vote_average: 7.2,
+        total_results: nil
+    )
+
+    static let popularWithOneMovie = PopularResult(
+        page: 1,
+        total_results: 1,
+        total_pages: 1,
+        results: [popularFirst])
+
+    static let popularWithTwoMovie = PopularResult(
+        page: 1,
+        total_results: 1,
+        total_pages: 1,
+        results: [popularFirst, popularSecond])
+}
--- iFoodCaseMovieTests/Mocks/Service/PopularServiceMock.swift
+++ iFoodCaseMovieTests/Mocks/Service/PopularServiceMock.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+@testable import iFoodCaseMovie
+
+class PopularServiceMock: PopularServiceProtocol {
+    var didCallLoadData = false
+    var isSuccess = false
+    var mockResult: [Popular] = PopularMock.popularWithOneMovie.results
+    
+    func loadData(completion: @escaping (Result<[iFoodCaseMovie.Popular], Error>) -> Void) {
+        didCallLoadData = true
+        isSuccess ? completion(.success(mockResult)) : completion(.failure(Error.self as! Error))
+    }
+}
