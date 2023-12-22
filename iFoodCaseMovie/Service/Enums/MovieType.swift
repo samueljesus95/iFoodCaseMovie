fa33e9d29cd47dbb09ba42cfb1277a2daac13817
Samuel Jesus <samueljesus@live.com>
2023-12-21 22:30:33 -0300

    feat: add new endPoints

--- iFoodCaseMovie/Service/Enums/MovieType.swift
+++ iFoodCaseMovie/Service/Enums/MovieType.swift
@@ -9,16 +9,25 @@ import Foundation
 
 enum MovieType {
     case popular
+    case nowPlaying
+    case upComing
+    case topRated
     
     var path: String {
         switch self {
         case .popular: return "/popular"
+        case .nowPlaying: return "/now_playing"
+        case .upComing: return "upcoming"
+        case .topRated: return "top_rated"
         }
     }
     
     var description: String {
         switch self {
         case .popular: return "Popular"
+        case .nowPlaying: return "Now Playing"
+        case .upComing: return "Upcoming"
+        case .topRated: return "Top Rated"
         }
     }
 }
