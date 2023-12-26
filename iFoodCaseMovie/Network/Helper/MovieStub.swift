5415a1d26352e4dd7532dd46a54c03fe0d042cc1
Samuel Jesus <samueljesus@live.com>
2023-12-25 23:49:24 -0300

    feat: add movie stub

---
+++ iFoodCaseMovie/Network/Helper/MovieStub.swift
@@ -0,0 +1,28 @@
+//
+//  MovieStub.swift
+//  iFoodCaseMovie
+//
+//  Created by Samuel on 25/12/23.
+//
+
+import Foundation
+
+struct MovieStub {
+    static let movieResult = Movie(
+        id: 76600,
+        poster_path: "/5ScPNT6fHtfYJeWBajZciPV3hEL.jpg",
+        adult: false,
+        overview: "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        release_date: "2022-12-14",
+        genre_ids: [878,28,12],
+        original_title: "Avatar: The Way of Water",
+        original_language: "en",
+        title: "Avatar: The Way of Water",
+        backdrop_path: "/tQ91wWQJ2WRNDXwxuO7GCXX5VPC.jpg",
+        popularity: 4109.455,
+        vote_count: 1001,
+        video: false,
+        vote_average: 8.1,
+        total_results: nil
+    )
+}
