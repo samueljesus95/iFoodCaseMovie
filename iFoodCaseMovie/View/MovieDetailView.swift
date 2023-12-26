696b8ad38ce39a51a401463835fc5929111a9c7c
Samuel Jesus <samueljesus@live.com>
2023-12-25 23:46:00 -0300

    feat: add movie detail view

---
+++ iFoodCaseMovie/View/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  iFoodCaseMovie
+//
+//  Created by Samuel on 25/12/23.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
---
+++ iFoodCaseMovie/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailViewModel.swift
+//  iFoodCaseMovie
+//
+//  Created by Samuel on 25/12/23.
+//
+
+import Foundation
